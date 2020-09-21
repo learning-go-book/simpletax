e71fb8a0fc149a978437b7ae84a32cfb81d70d6f
Jon Bodner <jon@bodnerfamily.com>
2020-09-21 00:00:03 -0400

    add new tax rates

--- taxes.go
+++ taxes.go
@@ -10,11 +10,13 @@ var taxTable = map[string]decimal.Decimal{
 	"12345": decimal.NewFromFloat(.08),
 	"19805": decimal.NewFromFloat(0),
 	"95472": decimal.NewFromFloat(0.09),
+	"23456": decimal.NewFromFloat(0.06),
+	"45678": decimal.NewFromFloat(0.0725),
 }
 
 func TaxForZip(zip string) (decimal.Decimal, error) {
 	tax, ok := taxTable[zip]
-	if !ok {
+	if ok {
 		return decimal.Decimal{}, fmt.Errorf("unknown zip: %s", zip)
 	}
 	return tax, nil
